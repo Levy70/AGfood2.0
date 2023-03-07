a8b32b54e3c672abd1c991ee52c26a80c58510db
Levis Mbaku Akanji <levis-mbaku.akanji@student.nbi-handelsakademin.se>
2023-03-07 10:42:25 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace AGfood2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chk_Röd = new System.Windows.Forms.CheckBox();
+            this.chk_7up = new System.Windows.Forms.CheckBox();
+            this.chk_Cola = new System.Windows.Forms.CheckBox();
+            this.chk_Fan = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chk_Ris = new System.Windows.Forms.CheckBox();
+            this.chk_Spa = new System.Windows.Forms.CheckBox();
+            this.chk_Banan = new System.Windows.Forms.CheckBox();
+            this.chk_Potat = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(834, 525);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 53);
+            this.button4.TabIndex = 34;
+            this.button4.Text = "Total";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(520, 616);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 53);
+            this.button3.TabIndex = 33;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(275, 625);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 53);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(191, 496);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(299, 26);
+            this.txtNum.TabIndex = 31;
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Location = new System.Drawing.Point(191, 442);
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(299, 26);
+            this.txtAdd.TabIndex = 30;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(191, 388);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(299, 26);
+            this.txtName.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(7, 504);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(178, 27);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Contact Number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(68, 442);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 27);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Address:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(89, 388);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 27);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Name:";
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(20, 625);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 53);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chk_Röd);
+            this.groupBox2.Controls.Add(this.chk_7up);
+            this.groupBox2.Controls.Add(this.chk_Cola);
+            this.groupBox2.Controls.Add(this.chk_Fan);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI Black", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(688, 162);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(349, 201);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Drinks";
+            // 
+            // chk_Röd
+            // 
+            this.chk_Röd.AutoSize = true;
+            this.chk_Röd.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_Röd.ForeColor = System.Drawing.Color.Black;
+            this.chk_Röd.Location = new System.Drawing.Point(6, 151);
+            this.chk_Röd.Name = "chk_Röd";
+            this.chk_Röd.Size = new System.Drawing.Size(128, 31);
+            this.chk_Röd.TabIndex = 4;
+            this.chk_Röd.Text = "Röd Vine";
+            this.chk_Röd.UseVisualStyleBackColor = true;
+            // 
+            // chk_7up
+            // 
+            this.chk_7up.AutoSize = true;
+            this.chk_7up.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_7up.ForeColor = System.Drawing.Color.Black;
+            this.chk_7up.Location = new System.Drawing.Point(6, 114);
+            this.chk_7up.Name = "chk_7up";
+            this.chk_7up.Size = new System.Drawing.Size(78, 31);
+            this.chk_7up.TabIndex = 3;
+            this.chk_7up.Text = "7Up";
+            this.chk_7up.UseVisualStyleBackColor = true;
+            // 
+            // chk_Cola
+            // 
+            this.chk_Cola.AutoSize = true;
+            this.chk_Cola.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_Cola.ForeColor = System.Drawing.Color.Black;
+            this.chk_Cola.Location = new System.Drawing.Point(6, 78);
+            this.chk_Cola.Name = "chk_Cola";
+            this.chk_Cola.Size = new System.Drawing.Size(134, 31);
+            this.chk_Cola.TabIndex = 2;
+            this.chk_Cola.Text = "Cola Zero";
+            this.chk_Cola.UseVisualStyleBackColor = true;
+            // 
+            // chk_Fan
+            // 
+            this.chk_Fan.AutoSize = true;
+            this.chk_Fan.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_Fan.ForeColor = System.Drawing.Color.Black;
+            this.chk_Fan.Location = new System.Drawing.Point(6, 44);
+            this.chk_Fan.Name = "chk_Fan";
+            this.chk_Fan.Size = new System.Drawing.Size(92, 31);
+            this.chk_Fan.TabIndex = 1;
+            this.chk_Fan.Text = "Fanta";
+            this.chk_Fan.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chk_Ris);
+            this.groupBox1.Controls.Add(this.chk_Spa);
+            this.groupBox1.Controls.Add(this.chk_Banan);
+            this.groupBox1.Controls.Add(this.chk_Potat);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Black", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(14, 148);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(329, 207);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Food";
+            // 
+            // chk_Ris
+            // 
+            this.chk_Ris.AutoSize = true;
+            this.chk_Ris.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_Ris.ForeColor = System.Drawing.Color.Black;
+            this.chk_Ris.Location = new System.Drawing.Point(3, 151);
+            this.chk_Ris.Name = "chk_Ris";
+            this.chk_Ris.Size = new System.Drawing.Size(71, 30);
+            this.chk_Ris.TabIndex = 3;
+            this.chk_Ris.Text = "Ris";
+            this.chk_Ris.UseVisualStyleBackColor = true;
+            // 
+            // chk_Spa
+            // 
+            this.chk_Spa.AutoSize = true;
+            this.chk_Spa.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_Spa.ForeColor = System.Drawing.Color.Black;
+            this.chk_Spa.Location = new System.Drawing.Point(3, 112);
+            this.chk_Spa.Name = "chk_Spa";
+            this.chk_Spa.Size = new System.Drawing.Size(135, 30);
+            this.chk_Spa.TabIndex = 2;
+            this.chk_Spa.Text = "Spaghetti";
+            this.chk_Spa.UseVisualStyleBackColor = true;
+            // 
+            // chk_Banan
+            // 
+            this.chk_Banan.AutoSize = true;
+            this.chk_Banan.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_Banan.ForeColor = System.Drawing.Color.Black;
+            this.chk_Banan.Location = new System.Drawing.Point(3, 78);
+            this.chk_Banan.Name = "chk_Banan";
+            this.chk_Banan.Size = new System.Drawing.Size(104, 30);
+            this.chk_Banan.TabIndex = 1;
+            this.chk_Banan.Text = "Banan";
+            this.chk_Banan.UseVisualStyleBackColor = true;
+            // 
+            // chk_Potat
+            // 
+            this.chk_Potat.AutoSize = true;
+            this.chk_Potat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_Potat.ForeColor = System.Drawing.Color.Black;
+            this.chk_Potat.Location = new System.Drawing.Point(6, 44);
+            this.chk_Potat.Name = "chk_Potat";
+            this.chk_Potat.Size = new System.Drawing.Size(109, 30);
+            this.chk_Potat.TabIndex = 0;
+            this.chk_Potat.Text = "Potatis";
+            this.chk_Potat.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(514, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 32);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "MENY";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1267, 113);
+            this.panel1.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(149, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AG EXPRESS FOOD";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1267, 690);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.txtAdd);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtAdd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chk_Röd;
+        private System.Windows.Forms.CheckBox chk_7up;
+        private System.Windows.Forms.CheckBox chk_Cola;
+        private System.Windows.Forms.CheckBox chk_Fan;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chk_Ris;
+        private System.Windows.Forms.CheckBox chk_Spa;
+        private System.Windows.Forms.CheckBox chk_Banan;
+        private System.Windows.Forms.CheckBox chk_Potat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace AGfood2._0
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Black", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(398, 480);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 76);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Black", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(398, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 76);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Order Now";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label5.Location = new System.Drawing.Point(324, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(442, 38);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Välkommen till Online MatButik";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 38);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "AG Express Food";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(957, 646);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+    }
+}
